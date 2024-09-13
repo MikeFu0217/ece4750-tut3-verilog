05d6825ca6b5f7cff2e1dacb382afcc02514c564
zf242 <zf242@cornell.edu>
2024-09-12 22:47:24 -0400

    finished tut3 until 4.9 randomtest

--- sim/tut3_verilog/regincr/test/RegIncr_test.py
+++ sim/tut3_verilog/regincr/test/RegIncr_test.py
@@ -33,8 +33,25 @@ def test_large( cmdline_opts ):
   ], cmdline_opts )
 
 # ''' TUTORIAL TASK ''''''''''''''''''''''''''''''''''''''''''''''''''''''
-# This test script is incomplete. As part of the tutorial you will add
-# another test case to test for overflow. Later you will add a test case
-# for random testing.
+def test_overflow( cmdline_opts ):
+  run_test_vector_sim( RegIncr(), [
+    ('in_   out*'),
+    [ 0x00, '?'  ],
+    [ 0xfe, 0x01 ],
+    [ 0xff, 0xff ],
+    [ 0x00, 0x00 ],
+  ], cmdline_opts )
 # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+#-------------------------------------------------------------------------
+# test_random
+#-------------------------------------------------------------------------
+import random
+def test_random( cmdline_opts ):
+  test_vector_table = [( 'in_', 'out*' )]
+  last_result = '?'
+  for i in range(20):
+    rand_value = Bits8( random.randint(0,0xff) )
+    test_vector_table.append( [ rand_value, last_result ] )
+    last_result = Bits8( rand_value + 1, trunc_int=True )
+  run_test_vector_sim( RegIncr(), test_vector_table, cmdline_opts )
